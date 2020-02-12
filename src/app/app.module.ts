ee7cd28757535634459c536f1ab84c10d7ba195a
praburajp <prabu420771@gmail.com>
2020-02-12 15:19:23 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CountdownTimerModule } from '../../node_modules/ngx-countdown-timer'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CountdownTimerModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
